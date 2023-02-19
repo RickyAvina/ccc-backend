2ea042ff31849890d5a3368a22c2d6d5f857d090
Enrique Avina <rickyavina@icloud.com>
2023-02-19 01:40:39 -0500

    Create user works, atomic operation, need documentsation and cleanup

--- src/handlers/create-user.js
+++ src/handlers/create-user.js
@@ -2,6 +2,8 @@
 
 // Create a DocumentClient that represents the query to add an item
 const dynamodb = require('aws-sdk/clients/dynamodb');
+const fetch = require('node-fetch');
+
 const docClient = new dynamodb.DocumentClient();
 
 // sam local invoke -e events/event-post-item putItemFunction
@@ -9,50 +11,183 @@ const docClient = new dynamodb.DocumentClient();
 // putItemFunction
 
 // Get the DynamoDB table name from environment variables
-const tableName = 'ccc-creator-studio-UserTable-JPN3IAVP0MY0'
+// const tableName = 'ccc-creator-studio-UserTable-JPN3IAVP0MY0'
 
+const tableName = process.env.USER_TABLE
+console.log("tableName: " + tableName);
 /**
  * A simple example includes a HTTP post method to add one item to a DynamoDB table.
  */
-exports.handler = async (event) => {
-    if (event.httpMethod !== 'POST') {
-        throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method.`);
-    }
-    // All log statements are written to CloudWatch
-    console.info('received:', event);
 
-    // Get id and name from the body of the request
-    const body = JSON.parse(event.body);
-    const {id, name, email, phone, bio} = body;
 
-    // const id = body.id;
-    // const name = body.name;
 
-    // Creates a new item, or replaces an old item with a new item
-    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
-    
-    let response = {};
 
+function addUserDynamoDB(id, name, email, phone_number, bio = "") {
+  return new Promise((resolve, reject) => async () => {
     try {
-        const params = {
-            TableName : tableName,
-            Item: { id : id, name: name, email, phone, bio }
-        };
-    
-        const result = await docClient.put(params).promise();
-    
-        response = {
-            statusCode: 200,
-            body: JSON.stringify(body)
-        };
+      const params = {
+        TableName: tableName,
+        Item: { id: id, name: name, email, phone_number, bio }
+      };
+
+      const result = await docClient.put(params).promise();
+      console.log("Success", result);
+
+      // response = {
+      //     statusCode: 200,
+      //     body: JSON.stringify({
+      //       id,
+      //       name,
+      //       email,
+      //       phone_number,
+      //       bio
+      //     })
+      // };
+
+      resolve()
     } catch (ResourceNotFoundException) {
-        response = {
-            statusCode: 404,
-            body: "Unable to call DynamoDB. Table resource not found."
-        };
+      // response = {
+      //     statusCode: 404,
+      //     body: "Unable to call DynamoDB. Table resource not found."
+      // };
+
+      reject("Unable to call DynamoDB. Table resource not found.")
+    }
+  });
+
+  // let response = {};
+
+
+  // All log statements are written to CloudWatch
+  // console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+  return response;
+}
+
+const retFailure = (data) => {
+  console.error("Error!", data)
+  return {
+    statusCode: 500,
+    body: JSON.stringify(data)
+  }
+}
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method.`);
+  }
+  // All log statements are written to CloudWatch
+  console.info('received:', event);
+  const body = JSON.parse(event.body);
+
+  const { client_id, email, password, connection, name, phone_number } = body;
+
+  const auth0ReqBody = {
+    client_id,
+    email,
+    password,
+    connection,
+    name,
+    user_metadata: {
+      phone_number
+    }
+  }
+
+  // Create user in auth0 db
+  try {
+    let response = await fetch('https://dev-86rvru3cjw5ztru0.us.auth0.com/dbconnections/signup', {
+      method: 'POST',
+      body: JSON.stringify(auth0ReqBody),
+      headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' }
+    });
+
+    const data = await response.json();
+    if (!response.ok || !(response.error == null || response.error == undefined) || data._id == null) {
+      return retFailure(data);
     }
 
-    // All log statements are written to CloudWatch
-    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
-    return response;
-};
+    const bio = "";
+
+    // Create user in dynamodb
+    const params = {
+      TableName: tableName,
+      Item: { id: data._id, name, email, phone_number, bio }
+    };
+
+    await docClient.put(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        id: data._id,
+        name,
+        email,
+        phone_number
+      })
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error)
+    }
+  }
+}
+
+
+
+
+  // }).then(response => response.json()
+  //   .then(data => {
+  //     if (!response.ok || !(response.error == null || response.error == undefined) || data._id == null) {
+  //       return retFailure(data);
+  //     }
+
+  //     console.log("************************************************************************************************")
+  //     // success, create user in dynamo db
+  //     addUserDynamoDB.then(()=>{
+  //       console.log("Success!");
+  //     }, (failMsg) => {
+  //       console.error(failMsg);
+  //     });
+
+  //     // const res = await addUserDynamoDB(client_id, name, email, phone_number, "");
+  //     // console.log(res)
+  //     // return res;
+
+  //   }).catch(err => retFailure(err))
+  // ).catch(err => retFailure(err));
+
+
+  // console.info(body);
+  // console.log(body);
+
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify("success")
+  // };
+
+  //   var xml = `<root>
+  //   <_parts>Hi</_parts>
+  //   <_parts>dfdfd</_parts>
+  // </root>`
+
+
+  // var parseString = require('xml2js').parseString;
+  // parseString(xml, function (err, result) {
+
+  //     console.info("parsed result", result);
+  //     console.log("parsed result", result);
+  //     return {
+  //       statusCode: 200,
+  //       body: JSON.stringify(result)
+  //   };
+  // });
+
+  // Get id and name from the body of the request
+  // const body = JSON.parse(event.body);
+  // const {id, name, email, phone, bio} = body;
+
+  // // Creates a new item, or replaces an old item with a new item
+  // // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
+
+
+// };
